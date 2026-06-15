91a0ca76b4b4043e0682a1f71eaa7b436fb4f6fa
Durga Sankar Sri Narayana Peddi <durgasankarsrinarayanapeddi@gmail.com>
2026-06-15 23:03:02 +0530

    secondcommit(included html,css and springboot structure)

---
+++ src/test/java/com/dssn/spring_begin/SpringBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dssn.spring_begin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
